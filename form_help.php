bd21a54a2a1a4f49f04d116bf5b59cee21e7d7c5
Daryya24 <daryq24@gmail.com>
2022-04-01 22:55:36 +0200

    Laba5 done

---
+++ form_help.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function buildTask(array $taskData): array
+{
+    return [
+        $taskData['Category'],
+        $taskData['Title'],
+        $taskData['Email'],
+        $taskData['Text']
+    ];
+}
